e5c68a67c1bd7e76b095fe7f9d20c090f191786c
Sanyek004 <92347497+Sanyek004@users.noreply.github.com>
2026-03-14 12:59:27 +1000

    Update Lab1.cpp
    This code is not the one which supposed to be there.

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,13 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-int main() {
-	printf("Hello world!");
+int main(int argc, char *argv[]) {
+	printf("Hello world!\n");
 	
-	double a = 1.0;
-	double b = 2.0;
-	double c = 3.0;
+	double a;
+	double b;
+	double c;
+	scanf("%lf %lf %lf", &a,&b,&c);
+	// Задание 1
+	// Формула дискрименанта / коэффициента квадратного уравнения: b^2 - 4ac
+	char symb = ' ';
+	while (symb != 'n')
+	{
+	
+		double D = pow(b,2.0) - 4 * a * c;
+		printf("%.1f", D);
+		if (D < 0)
+		{
+			printf("No solution!");
+		}
+		else
+		{
+			printf("\n");
+			double sqrt_1 = sqrt(D);
+			printf("%.1f", sqrt_1);
+			double x1 = (sqrt_1 - b) / (2 * a);
+			double x2 = (sqrt_1 - 2 * sqrt_1 - b) / (2 * a);
+			printf("\n");
+			printf("%.1f", x1);
+			printf("\n");
+			printf("%.1f", x2);
+			printf("\n");
+		}
 		
-	
+		printf("Continue?");
+		scanf(" %c", &symb);
+	}
 	return 0;
 }
