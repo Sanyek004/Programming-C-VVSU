83378e5002262715c5559437ad00d01d591dcc7a
Sanyek004 <92347497+Sanyek004@users.noreply.github.com>
2026-03-21 12:16:20 +1000

    Add Lab3.c
    We got string which need to change pair symbols: "a -> A"; "b -> B".

---
+++ Lab3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	const int COUNT_SYMBOL = 80;
+	int i = 0;
+	char str[COUNT_SYMBOL];
+	
+	printf("Input phrase: ");
+	fgets(str,sizeof(str),stdin);
+	
+	printf("%s", str);
+	printf("\n");
+	
+	for (i = 0; i < sizeof(str); i++)
+	{
+		if (str[i] == 'a')
+		{
+			str[i] = 'A';
+		}
+		else if (str[i] == 'b')
+		{
+			str[i] = 'B';
+		}
+	}
+	
+	printf("%s", str);
+	
+	return 0;
+}
